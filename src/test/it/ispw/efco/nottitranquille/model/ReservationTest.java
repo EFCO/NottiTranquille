1a19bd67b0e1e9f9a4868ae0de15bb14c8f03db5
Zanna-94 <emanuele.vannacci@gmail.com>
2016-04-08 16:37:51 +0200

    added junit

---
+++ src/test/it/ispw/efco/nottitranquille/model/ReservationTest.java
@@ -0,0 +1,16 @@
+package it.ispw.efco.nottitranquille.model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by emanuele on 08/04/16.
+ */
+public class ReservationTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
