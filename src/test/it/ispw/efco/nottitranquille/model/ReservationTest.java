bc20ff77e6afd3750c6c9337309e53565ed87c0b
Zanna-94 <emanuele.vannacci@gmail.com>
2016-07-17 17:21:46 +0200

    test case fixed

--- src/test/it/ispw/efco/nottitranquille/model/ReservationTest.java
+++ src/test/it/ispw/efco/nottitranquille/model/ReservationTest.java
@@ -27,33 +27,35 @@ public class ReservationTest {
 
     private ReservationBean bean;
 
+    private Location location;
+
     private Person tenant;
 
 
     @Before
     public void setUp() throws Exception {
 
-        // TODO: 17/07/16 create location
-        Location loc = LocationDAO.findByID(7l);    // location to book
+
+        location = LocationDAO.findByID(5l);    // location to book
 
         tenant = UserDAO.findBy("Zanna");
 
         /*  JavaBean for Location  */
         LocationBean locBean = new LocationBean();
-        locBean.setServices(loc.getServices());
-        locBean.setName(loc.getName());
-        locBean.setDescription(loc.getDescription());
-        locBean.setEnablesDate(loc.getAvailableDate());
-        locBean.setId(loc.getId().toString());
+        locBean.setServices(location.getServices());
+        locBean.setName(location.getName());
+        locBean.setDescription(location.getDescription());
+        locBean.setEnablesDate(location.getAvailableDate());
+        locBean.setId(location.getId().toString());
         locBean.setReservationType(ReservationType.WithConfirm.getText());
 
         /* JavaBean for Reservation */
         bean = new ReservationBean();
         bean.setUsername(tenant.getUsername());
 
-        bean.setStartDate("05-04-2017");
-        bean.setEndDate("11-04-2017");
-        bean.setPrice(loc.getPrice());
+        bean.setStartDate("06-10-2017");
+        bean.setEndDate("08-10-2017");
+        bean.setPrice(location.getPrice());
 
         // put the JavaBean of Location into the JavaBean of Reservation
         bean.setLocation(locBean);
@@ -67,35 +69,51 @@ public class ReservationTest {
         // and then call to the controller for the reservation instantiation
         boolean validate = bean.validate();
 
-        Reservation reservation = ReservationDAO.findByID(new Long(bean.getId()));
 
-        /* range of days */
         DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("dd-MM-yyyy");
 
-        DateTime da = DateTime.parse(bean.getStartDate(), dateTimeFormatter);
-        DateTime a = DateTime.parse(bean.getEndDate(), dateTimeFormatter);
+        DateTime bookingFrom = DateTime.parse(bean.getStartDate(), dateTimeFormatter);
+        DateTime bookingTo = DateTime.parse(bean.getEndDate(), dateTimeFormatter);
 
-        // through the reservation is created,e the days are still available
-        // because they are reserved only after the payment
-        boolean booked = reservation.getLocation().isBooked(new Interval(da, a));
+        Interval interval = new Interval(bookingFrom, bookingTo);
 
-        Assert.assertTrue(validate);
-        Assert.assertFalse(booked); // reserve period only after payment
-        Assert.assertEquals(reservation.getId(), new Long(bean.getId()), 0);
+        // test if the range of days is still available
+        if (location.isAvailable(interval)) {
+
+            Reservation reservation = ReservationDAO.findByID(new Long(bean.getId()));
+
+             /* range of days */
+
+            DateTime bookedFrom = DateTime.parse(bean.getStartDate(), dateTimeFormatter);
+            DateTime bookedTo = DateTime.parse(bean.getEndDate(), dateTimeFormatter);
+
+            // through the reservation is created,e the days are still available
+            // because they are reserved only after the payment
+            boolean booked = reservation.getLocation().isBooked(new Interval(bookedFrom, bookedTo));
+
+            Assert.assertTrue(validate);
+            Assert.assertFalse(booked); // reserve period only after payment
+            Assert.assertEquals(reservation.getId(), new Long(bean.getId()), 0);
 
 
-        /* decline and approve */
 
-        // create a list of reservation bean to emulate view
-        List<ReservationBean> res = new ArrayList<ReservationBean>();
-        res.add(bean);
-        ListReservationBean listBean = new ListReservationBean();
-        listBean.setBeans(res);
-        listBean.setnRes(1);
+             /* decline and approve */
 
-        listBean.approve(reservation.getId().toString());
+            if (reservation.getLocation().getReservationType() == ReservationType.WithConfirm) {
 
-        Assert.assertTrue(reservation.getState() == ReservationState.ToPay);
+                // create a list of reservation bean to emulate view
+                List<ReservationBean> res = new ArrayList<ReservationBean>();
+                res.add(bean);
+                ListReservationBean listBean = new ListReservationBean();
+                listBean.setBeans(res);
+                listBean.setnRes(1);
+
+                listBean.approve(reservation.getId().toString());
+
+                Assert.assertTrue(reservation.getState() == ReservationState.ToPay);
+            }
+
+        }
 
     }
 
